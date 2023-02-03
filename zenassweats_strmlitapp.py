bd0b51be11a8a86267d97d7486132f94255aad32
jfhoneys <111950666+jfhoneys@users.noreply.github.com>
2023-02-02 19:08:30 -0600

    Add confirmation of snowflake

--- zenassweats_strmlitapp.py
+++ zenassweats_strmlitapp.py
@@ -5,3 +5,11 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title("Zena Sweatsuit Line")
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
